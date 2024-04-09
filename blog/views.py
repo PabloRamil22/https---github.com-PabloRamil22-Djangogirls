4d82965e1e4a115233443615f868f3b2413978f4
PabloRamil22 <pabloramil2000@gmail.com>
2024-04-09 19:28:00 +0200

    a

--- blog/views.py
+++ blog/views.py
@@ -19,9 +19,6 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
-def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
 
 
 def post_new(request):
